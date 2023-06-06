5aa3d294669a7e7b628f0a0374d34b8f58388eaf
Екатерина Седова <katena.sedova@gmail.com>
2023-06-06 14:50:54 +0300

    подключение к бд, эндпоинты для создания пользователя и получения всех пользоватей

--- practice-backend/src/app.module.ts
+++ practice-backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
+import {User} from "./users/user.model";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: '.env'
+      }),
+      SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true,
+      }),
+      UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
---
+++ practice-backend/src/users/user.model.ts
@@ -0,0 +1,27 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript"
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+}
+
+@Table({tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    firstname: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    lastname: string;
+    @Column({type: DataType.STRING, allowNull: true})
+    sex: string;
+    @Column({type: DataType.STRING, allowNull: true})
+    profile_img: string;
+
+}
